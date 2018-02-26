116dba6195b324a69fa8afa945258b2978772813
Zac Willmington <zac.willmington@gmail.com>
2018-02-26 21:55:23 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,8 +8,8 @@ class Doctor
   end
 
   def add_appointment(new_appointment_intance)
-    @appointments << new_appointment_intance
     new_appointment_intance.doctor = self
+    @appointments << new_appointment_intance
   end
 
   def patients
