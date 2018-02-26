15b6e88e71a3f607e7dd1f6bca9b28ccfc3efdb0
Zac Willmington <zac.willmington@gmail.com>
2018-02-26 22:18:58 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,14 +1,21 @@
 class Patient
 
-  attr_accessor :name, :appointments, :doctors,:appointment
+  attr_accessor :name, :appointments, :appointment
 
   def initialize(name)
     @name = name
     @appointments = []
+    
   end
 
   def add_appointment(new_appointment_intance)
     new_appointment_intance.patient = self
     @appointments << new_appointment_intance
+  end
+
+  def doctors
+    self.appointments.collect do |doctor|
+        doctor.doctor
+    end
   end
 end
