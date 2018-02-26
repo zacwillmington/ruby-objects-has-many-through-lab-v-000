c7d6d945a068fa0cd85fda0f46bf823bbe95e0ce
Zac Willmington <zac.willmington@gmail.com>
2018-02-26 21:17:25 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,13 @@
 class Patient
 
-  attr_accessor :name
+  attr_accessor :name, :appointments
 
   def initialize(name)
     @name = name
+    @appointments = []
   end
 
+  def add_appointment(new_appointment_intance)
+    @appointments << new_appointment_intance
+  end
 end
